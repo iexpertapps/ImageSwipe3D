4ca071a797dd085a6ef52d3e10629d65f6db53f9
Hasaan Syed <muhammadhasaansyed@icloud.com>
2024-11-17 15:36:07 +0500

    Initial Commit

---
+++ ImageSwipe3D/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ImageSwipe3D
+//
+//  Created by Syed Zia ur Rehman on 17/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImageSwipe3D/ImageSwipe3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSwipe3DApp.swift
+//  ImageSwipe3D
+//
+//  Created by Syed Zia ur Rehman on 17/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageSwipe3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
